51d5e50e2b85f8d97946af2a250827da1b8ca2a6
wmc2tokyo <wmcooper2@yahoo.com>
2018-12-29 12:58:12 +0900

    Began renaming and restructuring of dirs/files. Rewrote "dirsandfiles.py".

--- src/directorysetup.py
+++ src/directorysetup.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-"""This module sets up the directory structure for webscraping.
-
-    This program creates files and directories in '/Volumes/YUUSHI/`
-
+"""Directory setup for Lyric program.
+    Creates files and directories in '<programroot>/src/'.
 """
 #stand lib
 from pathlib import Path
@@ -14,18 +11,21 @@ dirs = [
     CATEGORY_DIR,
     ARTIST_DIR,
     SONG_DIR,
-#    LYRIC_DIR
+    LYRIC_DIR
 ]
 
 files = [
     CATEGORY_ERRORS,
+    CATEGORY_FILE,
     CATEGORY_STATS,
     ARTIST_ERRORS,
+    ARTIST_FILE,
     ARTIST_STATS,
     SONG_ERRORS,
+    SONG_FILE,
     SONG_STATS,
-#    LYRIC_ERRORS,
-#    LYRIC_STATS
+    LYRIC_ERRORS,
+    LYRIC_STATS
 ]
 
 def check(list_):
@@ -33,43 +33,25 @@ def check(list_):
         Returns Boolean."""
     return all([Path(x).exists() for x in list_])
 
-def makedirs(list_):
-    """Makes directories in 'list_' if they don't exist.
-        Returns None."""
-    for path in list_:
-        if Path(path).exists():
-            pass
-        else:
-            Path(path).mkdir()
-            print("CREATED:\t", path)
+def message(path):
+    """Displays 'created' message in Terminal. Returns None."""
+    print("created: ", str(path))
 
-def makefiles(list_):
-    """Makes files in 'list_' if they don't exist.
-        Returns None."""
-    for path in list_:
-        if Path(path).exists():
-            pass
-        else:
-            Path(path).mkdir()
-            print("CREATED:\t", path)
+def setup(path):
+    """Makes path. Returns None."""
+    if str(path).endswith("txt"): path.touch()
+    else: path.mkdir()
+    message(path)
+
+def dir_file_setup(list_):
+    """Makes dirs and files. Returns None."""
+    for x in list_:
+        if not Path(x).exists(): setup(Path(x))
+        else: print("exists: ", x)
 
 #Main
 if __name__ == "__main__":
     print("--- DIRECTORY/FILE CHECK, START---")
-    if check(dirs):
-        print("Directories are in place.")
-    else:
-        makedirs(dirs)
-        print("Directories are in place.")
-    if check(files):
-        print("Files are in place.")
-    else:
-        makefiles(files)
-        print("Files are in place.")
+    if check(dirs+files): print("All dirs and files are in place.")
+    else: dir_file_setup(dirs+files)
     print("--- DIRECTORY/FILE CHECK, FINISH---")
-
-
-
-
-
-
